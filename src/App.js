49f8f619c4318fbd99c68bce0a6b5beb166d1f06
Asmaa Moallim <41068564+AsmaaMoallim@users.noreply.github.com>
2023-03-03 22:13:09 +0300

    loading page works

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 // import logo from './logo.svg';
-import './App.css';
-// import Navbar from "./components/navbar.js";
-// import Hero from "./components/hero.js";
+import "./App.css";
+import Navbar from "./components/navbar.js";
+import Hero from "./components/hero.js";
+import { useState, useEffect } from "react";
 import LoadingPage from "./loadingPage";
 
+function someRequest() {
+  //Simulates a request; makes a "promise" that'll run for 2.5 seconds
+  return new Promise((resolve) => setTimeout(() => resolve(), 2500));
+}
 function App() {
-  return (
-    <div className="App">
-      <LoadingPage />
-      {/* <Navbar />
-      <header>
-        <Hero/>   
-      </header> */}
-    </div>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    someRequest().then(() => {
+      setIsLoading(!isLoading);
+    });
+  });
+
+  if (isLoading) {
+    return <LoadingPage />;
+  } else {
+    return (
+      <div className="App">
+        <Navbar />
+        <header>
+          <Hero />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
